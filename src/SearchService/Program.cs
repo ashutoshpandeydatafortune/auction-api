89bb857e19b86daedaa957a8fe183504addd4ca9
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2024-04-13 18:20:47 +0530

    Added search service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,23 +1,28 @@
-using AuctionService.DB;
-using AuctionService.DB.Seeders;
-using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDBContext>(opt =>
-{
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
-});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 var app = builder.Build();
+
+await DB.InitAsync("auctionsdb", MongoClientSettings.FromConnectionString(
+    builder.Configuration.GetConnectionString("DefaultConnectionString"))
+);
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,14 +32,3 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-
-try
-{
-    DBInitializer.InitDb(app);
-}
-catch(Exception ex)
-{
-    Console.WriteLine("Cannot initialize seed: " + ex.Message);
-}
-
-app.Run();
