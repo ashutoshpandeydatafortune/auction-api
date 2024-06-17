ed3175ea552eb3b6e97deae42320b97276fbcc6c
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2024-06-17 17:41:49 +0530

    Created fetch wrapper

--- frontend/web-app/src/app/actions/auctionActions.ts
+++ frontend/web-app/src/app/actions/auctionActions.ts
@@ -1,15 +1,10 @@
 'use server'
 
+import { fetchWrapper } from "../../../lib/fetchWrapper";
 import { Auction, PagedResult } from "../../../types";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search${query}`);
-
-    if (res.status != 200) {
-        throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
+    return await fetchWrapper.get(`search${query}`);
 }
 
 export async function UpdateAuctionTest() {
